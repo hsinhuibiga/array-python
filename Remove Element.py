7c61d65e6b66ecba75523dfff681cf072b48c142
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-05 12:47:53 +0800

    Add files via upload

---
+++ Remove Element.py
@@ -0,0 +1,15 @@
+#Remove Element
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = 0
+        for i in xrange(len(nums)):
+            if nums[i] != val:
+                nums[length] = nums[i]
+                length += 1
+        return length
